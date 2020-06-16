30bf154dd2f6541b334f2cac379da863fdb873e3
jaybenaim <benaimjacob@gmail.com>
2020-06-15 20:32:19 -0400

    Updates

---
+++ precache-manifest.98bb0ce9f1ba6bb64f40bd1529f9fc15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0483bd290ffe7a39955fcf5bcd8048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3174588abdc2e25dc181",
+    "url": "/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "4e233f82d9c0fef22533",
+    "url": "/static/css/main.733a21e3.chunk.css"
+  },
+  {
+    "revision": "3174588abdc2e25dc181",
+    "url": "/static/js/2.e899e52d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e899e52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e233f82d9c0fef22533",
+    "url": "/static/js/main.dd13002e.chunk.js"
+  },
+  {
+    "revision": "232fa97e434cac34a11c",
+    "url": "/static/js/runtime-main.40b9b3db.js"
+  }
+]);
