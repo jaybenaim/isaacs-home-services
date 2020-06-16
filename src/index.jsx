c7ad8b8eb08c9da14fe36c6fdc62aa5fad497d7e
jaybenaim <benaimjacob@gmail.com>
2020-06-15 20:40:44 -0400

    deployed to gh-pages with server on heroku

--- src/index.jsx
+++ src/index.jsx
@@ -46,7 +46,7 @@ ReactSEO.startMagic(
 function renderDOM() {
   ReactDOM.render(
     <Provider store={store}>
-      <HashRouter history={history}>
+      <HashRouter>
         <ErrorBoundary>
           <App />
         </ErrorBoundary>
