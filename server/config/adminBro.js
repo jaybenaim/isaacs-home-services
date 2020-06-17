3055e385748fc33690d992bcb3c227453aadd481
jaybenaim <benaimjacob@gmail.com>
2020-06-17 01:33:08 -0400

    added adminbro actions

--- server/config/adminBro.js
+++ server/config/adminBro.js
@@ -2,6 +2,8 @@
 const AdminBro = require("admin-bro");
 const User = require("../models/User");
 const Service = require("../models/Service");
+const axios = require("axios");
+const { query } = require("express");
 
 AdminBro.registerAdapter(require("admin-bro-mongoose"));
 
@@ -17,6 +19,133 @@ const adminBro = new AdminBro({
   resources: [
     {
       resource: Service,
+      options: {
+        properties: {
+          firebaseID: {
+            isVisible: true,
+          },
+          image: {
+            isVisible: {
+              list: false,
+              edit: true,
+              show: true,
+            },
+          },
+        },
+        actions: {
+          new: {
+            before: async (request) => {
+              let data = request.payload;
+              // create service in firebase db
+              let serviceDetails = {
+                heading: data["details.heading"],
+                description: data["details.description"],
+                mainImage: data["details.mainImage"],
+              };
+              let serviceData = {
+                title: data.title,
+                subTitle: data.subTitle,
+                image: data.image,
+                shortDescription: data.shortDescription,
+                details: serviceDetails,
+              };
+
+              await axios
+                .post(
+                  "https://network-king-5740f.firebaseio.com/services.json",
+                  serviceData
+                )
+                .then((res) => {
+                  // set firebase id in service schema
+                  data.firebaseID = res.data.name;
+                })
+                .catch((err) => console.log(err));
+              return request;
+            },
+          },
+          edit: {
+            before: async (request) => {
+              let data = request.payload;
+              let { firebaseID } = data;
+
+              let serviceDetails = {
+                heading: data["details.heading"],
+                description: data["details.description"],
+                mainImage: data["details.mainImage"],
+              };
+              let serviceData = {
+                title: data.title,
+                subTitle: data.subTitle,
+                image: data.image,
+                shortDescription: data.shortDescription,
+                details: serviceDetails,
+              };
+
+              // update firebase
+              await axios
+                .patch(
+                  `https://network-king-5740f.firebaseio.com/services/${firebaseID}.json`,
+                  serviceData
+                )
+                .then((res) => {
+                  console.log("Service edited");
+                })
+                .catch((err) => console.log(err));
+              return request;
+            },
+          },
+          delete: {
+            before: async (request) => {
+              let recordId = request.params.recordId;
+              let firebaseId = "";
+
+              // find Service by id
+              Service.findById(recordId)
+                .then((res) => {
+                  let { firebaseID } = res;
+                  firebaseId = firebaseID;
+                })
+                .catch((err) => console.log(err));
+              // Delete service from firebase
+              await axios
+                .delete(
+                  `https://network-king-5740f.firebaseio.com/services/${firebaseId}.json`
+                )
+                .then((res) => {
+                  console.log(res.data, "Item Deleted");
+                })
+                .catch((err) => console.log(err));
+              return request;
+            },
+          },
+          bulkDelete: {
+            before: async (request) => {
+              let recordIds = request.query.recordIds.split(",");
+              // Get firebase ids from record Ids
+              recordIds.length >= 1 &&
+                (await recordIds.forEach((id) => {
+                  Service.findById(id)
+                    .then((data) => {
+                      axios
+                        .delete(
+                          `https://network-king-5740f.firebaseio.com/services/${data.firebaseID}.json`,
+                          request.payload
+                        )
+                        .then((res) => {
+                          console.log(res.data, "items deleted");
+                        })
+                        // catch firebase error
+                        .catch((err) => console.log(err));
+                    })
+                    // catch find error
+                    .catch((err) => console.log(err));
+                }));
+
+              return request;
+            },
+          },
+        },
+      },
     },
     // Show users on sidebar
     // {
