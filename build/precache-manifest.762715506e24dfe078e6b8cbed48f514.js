94331965c59afc8d6a8b8837cb46dcff2e329b23
jaybenaim <benaimjacob@gmail.com>
2020-06-18 15:50:28 -0400

    removed build for heroku

---
+++ build/precache-manifest.762715506e24dfe078e6b8cbed48f514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b566b7e0fb81975ef32328deacb0698",
+    "url": "/isaacs-home-services/index.html"
+  },
+  {
+    "revision": "8316b1637b1beebaaa63",
+    "url": "/isaacs-home-services/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "3ff31cfda6266c9349c0",
+    "url": "/isaacs-home-services/static/css/main.01ea3edc.chunk.css"
+  },
+  {
+    "revision": "8316b1637b1beebaaa63",
+    "url": "/isaacs-home-services/static/js/2.29d4e758.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/isaacs-home-services/static/js/2.29d4e758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ff31cfda6266c9349c0",
+    "url": "/isaacs-home-services/static/js/main.9a0aba15.chunk.js"
+  },
+  {
+    "revision": "14780989f08cfc17fd1e",
+    "url": "/isaacs-home-services/static/js/runtime-main.938063d9.js"
+  }
+]);
