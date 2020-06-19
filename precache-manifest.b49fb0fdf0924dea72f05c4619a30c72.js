209899d60560f74c5dcf5b6db4b22a4a0c5009e4
jaybenaim <benaimjacob@gmail.com>
2020-06-19 13:48:44 -0400

    Updates

--- precache-manifest.b49fb0fdf0924dea72f05c4619a30c72.js
+++ precache-manifest.b49fb0fdf0924dea72f05c4619a30c72.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "350b6f50ba909fa422e4c39b0bd6755d",
+    "revision": "d44f544e18c34dfde30b9f25279cc39e",
     "url": "/isaacs-home-services/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/isaacs-home-services/static/css/2.0161871a.chunk.css"
   },
   {
-    "revision": "9d6122f464be048ef64b",
-    "url": "/isaacs-home-services/static/css/main.d88af835.chunk.css"
+    "revision": "9a5463a0b57560b7d8b9",
+    "url": "/isaacs-home-services/static/css/main.d7f5a9d5.chunk.css"
   },
   {
     "revision": "bdbb9dd5fed762ff7804",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/isaacs-home-services/static/js/2.cf51abaf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9d6122f464be048ef64b",
-    "url": "/isaacs-home-services/static/js/main.1a318362.chunk.js"
+    "revision": "9a5463a0b57560b7d8b9",
+    "url": "/isaacs-home-services/static/js/main.d969ca8b.chunk.js"
   },
   {
     "revision": "14780989f08cfc17fd1e",
