ef484344d207dd9de2afa931cf88cda83ab905c1
jaybenaim <benaimjacob@gmail.com>
2020-06-18 23:02:39 -0400

    added padding for mobile offerItem

---
+++ build/precache-manifest.c834558f4563961a37b3017664322ab7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d38450b788d70a005f80424efa9aaa",
+    "url": "/isaacs-home-services/index.html"
+  },
+  {
+    "revision": "bdbb9dd5fed762ff7804",
+    "url": "/isaacs-home-services/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "bbd8f0583b187086599c",
+    "url": "/isaacs-home-services/static/css/main.e4170dde.chunk.css"
+  },
+  {
+    "revision": "bdbb9dd5fed762ff7804",
+    "url": "/isaacs-home-services/static/js/2.cf51abaf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/isaacs-home-services/static/js/2.cf51abaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd8f0583b187086599c",
+    "url": "/isaacs-home-services/static/js/main.0a3d2d6b.chunk.js"
+  },
+  {
+    "revision": "14780989f08cfc17fd1e",
+    "url": "/isaacs-home-services/static/js/runtime-main.938063d9.js"
+  },
+  {
+    "revision": "2aaa528af3ac851506b290be027f65d2",
+    "url": "/isaacs-home-services/static/media/grass-top-view.2aaa528a.jpg"
+  }
+]);
