23fc8d5ab5e6f294840d4cb582e3ea823cc3a47e
jaybenaim <benaimjacob@gmail.com>
2020-06-18 22:39:16 -0400

    shrank images for mobile

---
+++ build/precache-manifest.6e27d691f838114daea913f1ec90fc78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c271f2a30641884fc0e8a03234835787",
+    "url": "/isaacs-home-services/index.html"
+  },
+  {
+    "revision": "bdbb9dd5fed762ff7804",
+    "url": "/isaacs-home-services/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "063ffc61fee3025dd919",
+    "url": "/isaacs-home-services/static/css/main.75164b4e.chunk.css"
+  },
+  {
+    "revision": "bdbb9dd5fed762ff7804",
+    "url": "/isaacs-home-services/static/js/2.cf51abaf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/isaacs-home-services/static/js/2.cf51abaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063ffc61fee3025dd919",
+    "url": "/isaacs-home-services/static/js/main.4cca526b.chunk.js"
+  },
+  {
+    "revision": "14780989f08cfc17fd1e",
+    "url": "/isaacs-home-services/static/js/runtime-main.938063d9.js"
+  },
+  {
+    "revision": "2aaa528af3ac851506b290be027f65d2",
+    "url": "/isaacs-home-services/static/media/grass-top-view.2aaa528a.jpg"
+  }
+]);
