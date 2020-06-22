0bc6499e9908fc69bbf4672e99a13929998efc13
jaybenaim <benaimjacob@gmail.com>
2020-06-22 19:23:49 -0400

    image upload working

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import storage from "../Firebase/firebase";
+import { storage } from "../../firebase/firebase";
 
 const Admin = () => {
   const [showForm, setShowForm] = useState(false);
@@ -20,9 +20,36 @@ const Admin = () => {
     if (imageAsFile === "") {
       console.error(`not an image, the image file is a ${typeof imageAsFile}`);
     }
+
     const uploadTask = storage
       .ref(`/hero-images/${imageAsFile.name}`)
       .put(imageAsFile);
+    //initiates the firebase side uploading
+    uploadTask.on(
+      "state_changed",
+      (snapShot) => {
+        //takes a snap shot of the process as it is happening
+        console.log(snapShot);
+      },
+      (err) => {
+        //catches the errors
+        console.log(err);
+      },
+      () => {
+        // gets the functions from storage refences the image storage in firebase by the children
+        // gets the download url then sets the image from firebase as the value for the imgUrl key:
+        storage
+          .ref("hero-images")
+          .child(imageAsFile.name)
+          .getDownloadURL()
+          .then((fireBaseUrl) => {
+            setImageAsUrl((prevObject) => ({
+              ...prevObject,
+              imgUrl: fireBaseUrl,
+            }));
+          });
+      }
+    );
   };
   return (
     <div className="container">
@@ -32,11 +59,18 @@ const Admin = () => {
       >
         Change Hero Images
       </button>
+
       {showForm && (
-        <form onSubmit={handleFireBaseUpload}>
-          <input type="file" onChange={handleImageAsFile} />
-          <button>Save</button>
-        </form>
+        <>
+          <img
+            src={imageAsUrl.imgUrl}
+            alt={imageAsFile.name !== "" ? imageAsFile.name : "No image chosen"}
+          />
+          <form onSubmit={handleFireBaseUpload}>
+            <input type="file" onChange={handleImageAsFile} />
+            <button>Save</button>
+          </form>
+        </>
       )}
     </div>
   );
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/storage";
 
 var firebaseConfig = {
@@ -6,7 +6,7 @@ var firebaseConfig = {
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID,
@@ -14,8 +14,8 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 //analytics is optional for this tutoral
-firebase.analytics();
+// firebase.analytics();
 
 const storage = firebase.storage();
 
-export { storage, firebase as default };
+export { storage };
