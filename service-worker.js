56ce964e49c2ffd8ecf8b87ca541b7d513180a5f
jaybenaim <benaimjacob@gmail.com>
2020-06-23 11:52:28 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4aabdadc85a0b3abfac9d18a84a49da8.js"
+  "/precache-manifest.3e282690da52a2ae226b4ea788bd2c2f.js"
 );
 
 self.addEventListener('message', (event) => {
