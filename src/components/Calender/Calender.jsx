587c00736c862ac8d48d822513003fad077896a5
jaybenaim <benaimjacob@gmail.com>
2020-06-24 12:08:06 -0400

    addec contactInfo to calender

--- src/components/Calender/Calender.jsx
+++ src/components/Calender/Calender.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect } from "react";
+
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+
+import ContactInfo from "../ContactInfo/ContactInfo";
+
 import { getEvents, addToEvents } from "../../redux/actions/calenderActions";
 import { connect } from "react-redux";
+
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "../../assets/stylesheets/calender.css";
 
 const localizer = momentLocalizer(moment);
 
@@ -12,16 +18,18 @@ const MyCalendar = (props) => {
     props.getEvents();
     // eslint-disable-next-line
   }, []);
-
+  const { isAuthenticated, events } = props;
   return (
-    <div>
+    <div className="calender">
+      {!isAuthenticated && <h1 className="primary-font">Availability</h1>}
       <Calendar
         localizer={localizer}
-        events={props.events}
+        events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
       />
+      {!isAuthenticated && <ContactInfo />}
     </div>
   );
 };
@@ -29,6 +37,7 @@ const MyCalendar = (props) => {
 const mapStateToProps = (state) => {
   return {
     events: state.events.events,
+    isAuthenticated: state.auth.user.isAuthenticated,
   };
 };
 export default connect(mapStateToProps, { getEvents, addToEvents })(MyCalendar);
