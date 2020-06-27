27569e65bb87c45a77d221759dfad2771dd50e69
jaybenaim <benaimjacob@gmail.com>
2020-06-27 09:38:14 -0400

    added offerItem compoennt still good

--- src/components/WhatWeOffer/OfferItem.jsx
+++ src/components/WhatWeOffer/OfferItem.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import "../../assets/stylesheets/offerItem.css";
 
 const OfferItem = (props) => {
-  const { service } = props;
+  let { service, position } = props;
 
+  const gridClass = {
+    imgContainer: `img-container ${position % 2 === 0 ? "even" : "odd"}`,
+    details: `details ${(position + 1) % 2 === 0 ? "even" : "odd"}`,
+  };
   return (
     <div className="service-content">
-      <div className="img-container">
+      <div className={gridClass.imgContainer}>
         <img src={service.image} alt={service.title} className="img-thumb" />
 
         <div className="img-layover">
           <h5 className="secondary-font">{service.title}</h5>
         </div>
       </div>
-      <div className="details">
+      <div className={gridClass.details}>
         <h5 className="title secondary-font">{service.title}</h5>
 
         <p className="service-short-description secondary-font">
