4fee47426927d58a2003b16c5dd7d36f79d1932b
jaybenaim <benaimjacob@gmail.com>
2020-06-30 17:22:56 -0400

    added services for navbar on desktop

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.960772b0742eefac23a7b2d3dddcd2c8.js"
+  "/precache-manifest.0381520d6be57739313eb2dcee849bf5.js"
 );
 
 self.addEventListener('message', (event) => {
