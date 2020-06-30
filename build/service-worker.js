c3b0a5bbf3f54b835b064b69c6f3820631466035
jaybenaim <benaimjacob@gmail.com>
2020-06-30 17:57:57 -0400

    fixed calender styling

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0381520d6be57739313eb2dcee849bf5.js"
+  "/precache-manifest.e2e5ea70ddd992cb83e561d80e349a03.js"
 );
 
 self.addEventListener('message', (event) => {
