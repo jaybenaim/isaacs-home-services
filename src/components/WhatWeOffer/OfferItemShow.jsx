c4fd3cbbb95b7940b087995084f7bd938a1ae884
jaybenaim <benaimjacob@gmail.com>
2020-06-30 16:53:13 -0400

    display service show'

--- src/components/WhatWeOffer/OfferItemShow.jsx
+++ src/components/WhatWeOffer/OfferItemShow.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+import "../../assets/stylesheets/offerItemShow.css";
+import { Button } from "react-bootstrap";
 
-const OfferItemShow = ({
-  location: {
-    state: {
-      service,
-      service: { title, details },
-    },
-  },
-}) => {
-  if (!service) {
+const OfferItemShow = (props) => {
+  if (!props.location.state) {
     return <Redirect to="/" />;
   }
+
+  const {
+    location: {
+      state: {
+        service: {
+          title,
+          details: { heading, description, mainImage },
+        },
+      },
+    },
+  } = props;
+
   return (
-    <div className="" style={{ position: "relative", top: "120px" }}>
-      <h1>{title}</h1>
-      <h2>{details.heading}</h2>
+    <div
+      className="offer-item-show-page"
+      style={{ position: "relative", top: "120px" }}
+    >
+      <div className="left-column">
+        <img src={mainImage} alt={title} />
+      </div>
+      <div className="center-column">
+        <h1 className="primary-font">{title}</h1>
+        <h2 className="secondary-font primary-font-color">{heading}</h2>
+        <p className="secondary-font">{description}</p>
+        <Button variant="info">
+          <a href="tel:6472295873">Book A Quote</a>
+        </Button>
+      </div>
+      <div className="right-column"></div>
     </div>
   );
 };
